396837cecdbca8a3286cc6dd66ebb794f7dfb8f1
Indra2201020002 <bioohazard2@gmail.com>
2023-10-25 19:48:31 +0800

    tugas

--- app/Http/Controllers/MKController.php
+++ app/Http/Controllers/MKController.php
@@ -6,12 +6,37 @@ use Illuminate\Http\Request;
 
 class MKController extends Controller
 {
+    private $data = [
+        [
+            'nim' => "123456",
+            'jurusan' => "TI",
+            'mk' => "Web Programming",
+        ],
+        [
+            'nim' => "234567",
+            'jurusan' => "TI",
+            'mk' => "Sensor And Transduser",
+        ],
+        [
+            'nim' => "345678",
+            'jurusan' => "SK",
+            'mk' => "Digital Image Processing",
+        ],
+        [
+            'nim' => "456789",
+            'jurusan' => "DGM",
+            'mk' => "electronic",
+        ],
+    ];
+
     public function index()
     {
+        return view('mk.index', ['data' => $this->data]);
     }
 
     public function create()
     {
+        return view('mk.create');
     }
 
     public function show($id)
