2c1497e546a045db9f020cfedffb5f4623ea70b1
Indra2201020002 <bioohazard2@gmail.com>
2023-11-11 18:01:36 +0800

    menambah show

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -58,7 +58,45 @@ class MahasiswaController extends Controller
         return view('mahasiswa.edit', ['data' => $mhs, 'id' => $id, 'jurusan' => $jurusan]);
     }
 
+    public function store(Request $request)
+    {
+        DB::table ('mhs')->insert([
+            'nim'=>$request->nim,
+            'nama'=>$request->nama,
+            'jurusan_id'=>$request->jurusan,
+        ]);
+        return redirect(url('/mahasiswa'));
+    }
+
+    public function update(Request $request,$id)
+    {
+        DB::table ('mhs')
+        ->where('id',$id)
+        ->update([
+            'nim' => $request->nim,
+            'nama' => $request->nama,
+            'jurusan_id' => $request->jurusan,
+        ]);
+        return redirect(url('/mahasiswa'));
+    }
+
+    public function destroy($id)
+    {
+        DB::table ('mhs')
+        ->where('id',$id)
+        ->delete();
+
+        return redirect(url('/mahasiswa'));
+    }
+
     public function show($id)
     {
+        $mhs = DB::table('mhs')
+        ->select("mhs.id", "nim", "mhs.nama", "jurusan_id", "jurusan.nama AS jurusan_nama")
+        ->join('jurusan', 'jurusan.id', '=', 'mhs.jurusan_id')
+        ->where('mhs.id', $id)
+        ->first();
+
+        return view('mahasiswa.show', ['data' => $mhs]);
     }
 }
